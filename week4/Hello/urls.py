91c46c3d6074344e5258e70abfaf9d6b19355a76
KyleLongaker <79126650+KyleLongaker@users.noreply.github.com>
2021-10-08 13:55:14 -0600

    url schema

--- week4/Hello/urls.py
+++ week4/Hello/urls.py
@@ -5,5 +5,6 @@ from django.views.generic import TemplateView
 urlpatterns = [
     
     path('home', TemplateView.as_view(template_name="home.html")),
+    path('', TemplateView.as_view(template_name="home.html")),
     
 ]
--- week4/bacs350/urls.py
+++ week4/bacs350/urls.py
@@ -5,5 +5,6 @@ from django.views.generic import TemplateView
 urlpatterns = [
     path('about', TemplateView.as_view(template_name="about.html")),
     path('home', TemplateView.as_view(template_name="home.html")),
-    path('',include('Hello.urls')),
+    # path('',include('Hello.urls')),
+    path('', TemplateView.as_view(template_name="home.html")),
 ]

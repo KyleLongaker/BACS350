f961ae8c42803d77f6412a9a4db87bbf5a227ca8
KyleLongaker <79126650+KyleLongaker@users.noreply.github.com>
2021-11-03 15:02:26 -0600

    hero model

--- week4/hero/models.py
+++ week4/hero/models.py
@@ -1,3 +1,8 @@
 from django.db import models
-
+class Hero(models.Model):
+    name=models.CharField(max_length=100)
+    description = models.TextField()
+    
+    def __str__(self):
+      return (f"{self.name}")
 # Create your models here.

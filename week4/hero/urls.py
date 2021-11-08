a5efe2b6a761d242749677cf6e537e0c19af94fa
KyleLongaker <79126650+KyleLongaker@users.noreply.github.com>
2021-11-08 14:48:03 -0700

    bacs350

--- week4/hero/urls.py
+++ week4/hero/urls.py
@@ -2,4 +2,5 @@ from django.urls import path
 from . import views
 urlpatterns =[
     path('', views.indexview.as_view(), name ='index'),
+    path('hero/', views.heroListView.as_view(), name ='hero'),
 ]
--- week4/hero/views.py
+++ week4/hero/views.py
@@ -1,5 +1,11 @@
+from django.db import models
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView
+from . import models
 # Create your views here.
 class indexview(TemplateView):
     template_name ='index.html'
+
+class heroListView(ListView):
+    model = models.Hero
+    template_name ='hero_lists.html'
